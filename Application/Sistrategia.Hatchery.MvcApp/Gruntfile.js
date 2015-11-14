8fb41b250f3f0f3bdc33d00ffc7bdfb9635302d3
José Ernesto Ocampo Cicero <ernesto@sistrategia.com>
2015-11-14 07:34:21 -0600

    Generating standard and min versions of site.css and bootstrap.css

--- Application/Sistrategia.Hatchery.MvcApp/Gruntfile.js
+++ Application/Sistrategia.Hatchery.MvcApp/Gruntfile.js
@@ -12,21 +12,22 @@
                     ]
                 },
                 files: {
-                    'Content/bootstrap.debug.css': 'Content/bootstrap.less',
-                    'Content/site.debug.css': 'Content/site.less',
+                    'Content/bootstrap.css': 'Content/bootstrap/bootstrap.less',
+                    'Content/site.css': 'Content/site.less',
                 }
             },
             production: {
                 options: {
                     compress: true,
+                    sourceMap: true,
                     relativeUrls: true,
                     plugins: [
                             new (require('less-plugin-autoprefix'))({ browsers: ["last 2 versions"] })
                     ]
                 },
                 files: {
-                    'Content/bootstrap.css': 'Content/bootstrap.less',
-                    'Content/site.css': 'Content/site.less',
+                    'Content/bootstrap.min.css': 'Content/bootstrap/bootstrap.less',
+                    'Content/site.min.css': 'Content/site.less',
                 }
             }
         },
