60a1c9d80d099e3065c8d2bc2366a475584e2d81
Ernesto Ocampo <ernesto@sistrategia.com>
2022-08-27 13:51:21 -0500

    Add new files

---
+++ src/Applications/Sistrategia.Hatchery.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sistrategia.Hatchery.WebApp.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index() {
+        //return View();
+        return View("Welcome");
+    }
+
+    public IActionResult Welcome() {
+        return View();
+    }
+}
